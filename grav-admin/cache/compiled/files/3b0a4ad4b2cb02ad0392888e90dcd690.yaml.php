fe6dff211cc0c405a29d43f52e6fb4d3b2c0eb6a
betojaton <betojaton@gmail.com>
2020-09-05 20:47:49 -0300

    GRav cms

---
+++ grav-admin/cache/compiled/files/3b0a4ad4b2cb02ad0392888e90dcd690.yaml.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/xampp/htdocs/apukuntur-ong_com_ar/grav-admin/user/accounts/adm-beto.yaml',
+    'modified' => 1599246026,
+    'data' => [
+        'state' => 'enabled',
+        'email' => 'betojaton@gmail.com',
+        'fullname' => 'Roberto',
+        'title' => 'Administrador',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$FLd0fsn0GSsehL6fzSDJN.eMdkVOfT32Z.X1RYpjBquL8/I66x69K',
+        'pw_resets' => [
+            0 => 1599246026
+        ],
+        'reset' => 'cd3d8cf4f6ec67f2d325a9cea71812ba::1599850826'
+    ]
+];
